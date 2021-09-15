dcea3a851ea75dd969ef93378c289b945268f4dd
Torje Markussen <torjemarkussen@gmail.com>
2021-09-15 18:28:06 +0200

    Added Exceptionhandling
    Throws IllegalArgumentException if name is empty or amount/unit is < 0.0

--- sultn/core/src/main/java/core/Ingredient.java
+++ sultn/core/src/main/java/core/Ingredient.java
@@ -13,10 +13,20 @@ public class Ingredient {
     private double amount;
 
     // Constructor
-    public Ingredient(String aName, double aAmount, String aUnit) {
-        name = aName;
-        amount = aAmount;
-        unit = aUnit;
+    public Ingredient(String name, double amount, String unit) {
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("Name has no content.");
+        }
+        if (amount < 0.0){
+            throw new IllegalArgumentException("Amount must be a positive number (0 or larger).");
+        }
+        if (unit.isEmpty()){
+            throw new IllegalArgumentException("Unit must be defined.");
+        }
+
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
     }
 
     // Gets
@@ -33,15 +43,15 @@ public class Ingredient {
     }
 
     // Sets
-    public void setIngredientName(String newName){
-        name = newName;
+    public void setIngredientName(String name){
+        this.name = name;
     }
 
-    public void setIngredientAmount(double newAmount){
-        amount = newAmount;
+    public void setIngredientAmount(double amount){
+        this.amount = amount;
     }
 
-    public void setIngredientUnit(String newUnit){
-        unit = newUnit;
+    public void setIngredientUnit(String unit){
+        this.unit = unit;
     }
 }
