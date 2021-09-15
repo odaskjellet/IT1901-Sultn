be0854ca65c14fd707af2174496390f24425546a
Torje Markussen <torjemarkussen@gmail.com>
2021-09-15 15:53:17 +0200

    Made Ingredient class
    Uses a constructor to set variables on initialize. Has "sets" and "gets"

--- sultn/core/src/main/java/core/Ingredient.java
+++ sultn/core/src/main/java/core/Ingredient.java
@@ -1,5 +1,47 @@
 package core;
 
+/**
+ * Ingredient class with constructor for name, amount and unit.
+ * Contains "getters" and "setters" for each of the three variables.
+*/
+
 public class Ingredient {
     
+    // Variables
+    private String name;
+    private String unit;
+    private double amount;
+
+    // Constructor
+    public Ingredient(String aName, double aAmount, String aUnit) {
+        name = aName;
+        amount = aAmount;
+        unit = aUnit;
+    }
+
+    // Gets
+    public String getIngredientName(){
+        return name;
+    }
+
+    public double getIngredientAmount(){
+        return amount;
+    }
+
+    public String getIngredientUnit(){
+        return unit;
+    }
+
+    // Sets
+    public void setIngredientName(String newName){
+        name = newName;
+    }
+
+    public void setIngredientAmount(double newAmount){
+        amount = newAmount;
+    }
+
+    public void setIngredientUnit(String newUnit){
+        unit = newUnit;
+    }
 }
