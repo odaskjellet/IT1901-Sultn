b21e1035207f63a52eb6bf55e60b391e296ccee6
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-22 16:57:12 +0200

    added SultnModule.java handling the serializer classes

---
+++ sultn/json/src/main/java/json/SultnModule.java
@@ -0,0 +1,24 @@
+package json;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import core.CookBook;
+import core.Ingredient;
+import core.Recipe;
+
+@SuppressWarnings("serial")
+public class SultnModule extends SimpleModule {
+    private static final String NAME = "SultnModule";
+
+    public SultnModule(boolean deepSultnModelSerializer) {
+        super(NAME, Version.unknownVersion());
+        addSerializer(Ingredient.class, new IngredientSerializer());
+        addSerializer(Recipe.class, new RecipeSerializer());
+        // addSerializer(CookBook.class, new CookBookSerializer());
+    }
+
+    public SultnModule() {
+        this(true);
+    }
+}
