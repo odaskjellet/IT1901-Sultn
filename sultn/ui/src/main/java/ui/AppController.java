fb746dd25f0efd0d58090a9c77f633ad62c100bc
Robert Thor Arnarsson <roberarn@stud.ntnu.no>
2021-09-23 09:43:56 +0000

    Update sultn/ui/src/main/resources/ui/Sultn.fxml, sultn/ui/src/main/resources/ui/SultnForm.fxml, sultn/ui/src/main/java/ui/App.java, sultn/ui/src/main/java/ui/AppController.java files

--- sultn/ui/src/main/java/ui/AppController.java
+++ sultn/ui/src/main/java/ui/AppController.java
@@ -1,16 +1,5 @@
 package ui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-
 public class AppController {
 
-    @FXML TableView recipeTable;
-    @FXML TableColumn recipePictures;
-    @FXML TableColumn recipeNames;
-    @FXML Button btnAddRecipe;
-
-    
 }
