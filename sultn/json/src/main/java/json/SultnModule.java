779a709c465b9c53cc9acac9d49a0bd703fe4d07
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-24 12:23:19 +0200

    added main function in SultnModule printing correct json to terminal

--- sultn/json/src/main/java/json/SultnModule.java
+++ sultn/json/src/main/java/json/SultnModule.java
@@ -1,6 +1,12 @@
 package json;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import core.Cookbook;
@@ -20,5 +26,30 @@ public class SultnModule extends SimpleModule {
 
     public SultnModule() {
         this(true);
+    }
+
+    public static void main(String[] args) {
+        Ingredient ing = new Ingredient("Eggs", 2.0, "pcs");
+        List<Ingredient> ings = new ArrayList<Ingredient>(Arrays.asList(ing));
+        List<String> instr = new ArrayList<String>(Arrays.asList( 
+            "Fry guanciale in a pan.",
+            "Cook pasta.",
+            "Make slurry of eggs, parmigiano, and pepper.",
+            "Add al dente pasta to frying pan along with slurry",
+            "Stir constantly",
+            "Add salt to taste"));
+        
+        Recipe recipe = new Recipe("Carbonara", 123, ings, instr);
+        Cookbook cookbook = new Cookbook();
+        cookbook.addRecipe(recipe);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new SultnModule());
+
+        try {
+            System.out.println(mapper.writeValueAsString(cookbook));
+        } catch (JsonProcessingException e) {
+            System.err.println(e);
+        }
     }
 }
