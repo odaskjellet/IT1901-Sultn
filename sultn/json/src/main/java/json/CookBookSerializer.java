0338248fb4d560ecfa0d0d84cbdfe2c4faea016f
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-24 12:20:47 +0200

    updated recipe serializer

--- sultn/json/src/main/java/json/CookBookSerializer.java
+++ sultn/json/src/main/java/json/CookBookSerializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import core.Cookbook;
+import core.Recipe;
 
 public class CookBookSerializer extends JsonSerializer<Cookbook> {
     
@@ -14,6 +15,11 @@ public class CookBookSerializer extends JsonSerializer<Cookbook> {
     public void serialize(Cookbook cookBook, JsonGenerator jsonGen, SerializerProvider serializerProvider) throws IOException {
         jsonGen.writeStartObject();
         
+        jsonGen.writeArrayFieldStart("recipes");
+        for (Recipe r : cookBook.getRecipes()) {
+            jsonGen.writeObject(r);
+        }
+        jsonGen.writeEndArray();
 
         jsonGen.writeEndObject();
     }
