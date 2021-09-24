02b1cb754e305a17d21d62fb25114a75cc2d8013
roberarn <roberarn@stud.ntnu.no>
2021-09-24 12:01:34 +0200

    Corrected pom, changed Cookbook constructor, made Recipe constructor public, started adding controller methods

--- sultn/ui/src/main/java/ui/SultnController.java
+++ sultn/ui/src/main/java/ui/SultnController.java
@@ -1,5 +1,88 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import core.Cookbook;
+import core.Ingredient;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
 public class SultnController {
 
+    
+    private Cookbook cookbook;
+    
+    @FXML BorderPane bPane;
+
+    public void initialize(){
+        cookbook = new Cookbook();
+        //cookbook.makeNewRecipe(saveHandler.load());
+
+
+        // Mistenker at vi ikke kan launche med tom kokebok
+        List<Ingredient> testIngredients = new ArrayList<>();
+        List<String> testInstructions = new ArrayList<>();
+        Ingredient testTomat = new Ingredient("tomat", 2, "stk");
+        testIngredients.add(testTomat);
+        testInstructions.add("kok tomat");
+        cookbook.makeNewRecipe("Test", testInstructions, testIngredients);
+        // -------------------------------------
+
+
+        createRecipeList();
+    }
+
+    public static class HBoxCell extends HBox {
+        Label label = new Label();
+        Button button = new Button();
+        
+        HBoxCell(String recipeName, int id) {
+             super();
+
+             label.setText(recipeName);
+             label.setMaxWidth(Double.MAX_VALUE);
+             HBox.setHgrow(label, Priority.ALWAYS);
+
+             button.setText("Open");
+             button.setId("" + id); //should be the same as recipe id.
+
+            // button.setOnAction(value); //must put on an "OnAction" or an action event 
+
+             this.getChildren().addAll(label, button);
+        }
+   }
+
+
+   private void createRecipeList(){
+
+    List<HBoxCell> list = new ArrayList<>();
+
+    for(int key : cookbook.getRecipeMap().keySet()) {
+        list.add(new HBoxCell(cookbook.getRecipeMap().get(key).getName(), 
+        cookbook.getRecipeMap().get(key).getId()));
+    }
+
+    ListView<HBoxCell> recipeView = new ListView<HBoxCell>();
+    ObservableList<HBoxCell> observableList = FXCollections.observableList(list);
+    recipeView.setItems(observableList);
+
+    bPane.setCenter(recipeView);
+
+    /*ListView recipeView = new ListView(); 
+    List<Recipe> recipeList = cookbook.getRecipes(); //we assume this works
+    for(Recipe recipe: recipeList) {
+        recipeView.getItems().add(recipe);
+    }*/
+
+
+    }
 }
