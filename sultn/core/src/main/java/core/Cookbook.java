6343b3df6e6d8c449341c7e14b91026dbcbd0d80
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-27 12:07:20 +0200

    readded files with correct case

---
+++ sultn/core/src/main/java/core/Cookbook.java
@@ -0,0 +1,90 @@
+package core;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+The cookbook class is a collection with recipes, and contains methods that add,
+delete, edit and make new recipes. 
+The constructor contains a hash map with recipes and their ids.
+*/
+
+
+public class Cookbook {
+    
+    private HashMap<Integer, Recipe> recipeMap = new HashMap<>();
+    //variabel for id-ene til recipes
+    private int counter = 0;  
+
+    //Constructors
+    public Cookbook() {
+        
+    }
+
+    public Cookbook(HashMap<Integer, Recipe> recipeMap) { 
+        this.recipeMap = recipeMap;
+    }
+
+    //Getters
+    public int getCounter() {
+        return this.counter;
+    }
+
+    public HashMap<Integer, Recipe> getRecipeMap() {
+        return this.recipeMap;
+    }
+
+
+    //Setters
+    public void setCounter(int newCounter) {
+        this.counter = newCounter;
+    }
+
+     
+    //returnerer en liste med alle id-ene
+    public List<Integer> getIds() { 
+        return new ArrayList<>(recipeMap.keySet());
+    }
+
+    //returnerer en liste med alle oppskriftene 
+    public List<Recipe> getRecipes() {
+        List<Recipe> allRecipes = new ArrayList<Recipe>(recipeMap.values());
+        return allRecipes;
+    }
+
+    //oppretter en ny oppskrift og legger den til
+    public void makeNewRecipe(String name, List<String> instructions, List<Ingredient> ingredients) { 
+        int id = counter;
+        Recipe newRecipe = new Recipe(name, id, ingredients, instructions);
+        addRecipe(newRecipe);
+        counter++;
+    }
+    
+    //fjerner en oppskrift
+    public void deleteRecipe(Recipe recipe) { 
+        int id = recipe.getId();
+
+        if(recipeMap.containsKey(id)) {
+            recipeMap.remove(id); 
+        }
+        else {
+            throw new IllegalArgumentException("Recipe " + recipe.getName() + " not found. Invalid id.");
+        }
+    }
+        
+    //legger til en oppskrift i HashMap
+    public void addRecipe(Recipe recipe) {
+        int id = recipe.getId();
+        recipeMap.put(id, recipe);
+    }
+    
+    //denne burde kunne kalle på andre funksjoner slik at man kan endre på beskrivelsen og ingredienser. 
+    //public Recipe editRecipe(Recipe recipe) {  
+        //recipe.edit(); //gjøres i recipe
+        //return recipe;
+    //}
+        
+
+}
+
---
+++ sultn/core/src/main/java/core/Recipe.java
@@ -0,0 +1,93 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/*
+Class represents one recipe containing name of recipe, id for CookBook class' hash map,
+array of ingredients objects and array of string instructions.
+*/
+
+public class Recipe {
+    private String name;
+    private int id;
+    private List<Ingredient> ingredients = new ArrayList<>();
+    private List<String> instructions = new ArrayList<>();
+
+
+    public Recipe(String name, int id, List<Ingredient> ingredients, List<String> instructions) {
+        validateString(name);
+        validateInt(id);
+        validateIngArray(ingredients);
+        validateStrArray(instructions);
+
+        this.name = name;
+        this.id = id;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public List<String> getInstructions() {
+        return instructions;
+    }
+
+    @Override
+    public String toString(){
+        String empt = "";
+        for(String str :this.getInstructions()){
+            empt = empt.concat(str + "/n");
+        }
+        return empt;
+    }
+
+    // Setters
+    public void setName(String newName) {
+        validateString(newName);
+        this.name = newName;
+    }
+
+    public void setId(int newId) {
+        validateInt(newId);
+        this.id = newId;
+    }
+    
+    public void setIngredients(List<Ingredient> newIngredients) {
+        validateIngArray(newIngredients);
+        this.ingredients = newIngredients;
+    }
+
+    public void setInstructions(List<String> newInstructions) {
+        validateStrArray(newInstructions);
+        this.instructions = newInstructions;
+    }
+
+    // Validation
+    private void validateString(String toValidate) {
+        if (toValidate.isEmpty()) throw new IllegalArgumentException("Name cannot be empty.");
+    }
+    
+    private void validateInt(int toValidate) {
+        if (toValidate < 0) throw new IllegalArgumentException("ID cannot be negative.");
+    }
+
+    private void validateIngArray(List<Ingredient> toValidate) {
+        if (toValidate.size() == 0) throw new IllegalArgumentException("Recipe must contain at least one ingredient.");
+    }
+
+    private void validateStrArray(List<String> toValidate) {
+        if (toValidate.size() == 0) throw new IllegalArgumentException("Recipe must contain at least one instruction.");
+    }
+}
